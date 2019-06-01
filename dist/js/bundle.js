e7b631d20cd4b1393ac4ada06e0c5001e85ba2a8
Nikita <pishkin1112@gmail.com>
2019-06-01 17:24:03 +0300

    #2 commit(send form)

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -94,94 +94,99 @@
 /***/ (function(module, exports) {
 
 const form = () => {
+
     // Отправку формы на сервер 
-  let message = {
-    loading: 'Идет отправка.',
-    success: 'Спасибо! Скоро мы с вам свяжемся',
-    failure: "ЧТо-то пошло не так..."
-  };
+    // let message = {
+    //     loading: 'Идет отправка..',
 
-  let statusMessge = document.createElement('div');
-  statusMessge.classList.add('status');
+    // };
 
-  let img = document.createElement('img'),
-      popup = document.querySelectorAll('.popup-content');
+    let statusMessge = document.createElement('div');
+    statusMessge.classList.add('status');
 
+    let statusImg = document.createElement('img'),
+        popup = document.querySelectorAll('.popup-content');
 
 
-  const sendForm = (elem) => {
 
-    elem.appendChild(statusMessge);
 
-    let promise = new Promise( (resolve, reject) => {
+    const sendForm = (elem) => {
 
-      let request = new XMLHttpRequest();
+        elem.appendChild(statusMessge);
 
-      request.open('POST', '../src/server.php');
-      request.setRequestHeader('Contetnt-type', 'application/json; charset=utf-8');
+        let promise = new Promise((resolve, reject) => {
 
-      let formData = new FormData(elem),
-          obj = {};
+            let request = new XMLHttpRequest();
 
-      formData.forEach((value, key) => {
-        obj[key] = value;
-      });
+            request.open('POST', '../src/server.php');
+            request.setRequestHeader('Contetnt-type', 'application/json; charset=utf-8');
 
-      let json = JSON.stringify(obj);
-      request.send(json);
+            let formData = new FormData(elem),
+                obj = {};
 
-      request.addEventListener('readystatechange', () => {
-        if (request.readyState < 4) {
-        statusMessge.innerHTML = message.loading; 
-        } else if (request.readyState === 4 && request.status == 200) {
-          resolve();
-        } else {       
-          reject();
+            formData.forEach((value, key) => {
+                obj[key] = value;
+            });
+
+            let json = JSON.stringify(obj);
+            request.send(json);
+
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState < 4) {
+                    statusMessge.innerHTML = 'Идет отправка..';
+                } else if (request.readyState === 4 && request.status == 200) {
+                    resolve();
+                } else {
+                    reject();
+                }
+            });
+        });
+
+        return promise;
+    };
+
+    const clearInput = (elem) => {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].value = '';
         }
-      });
+    };
+
+    const putImg = (target, source) => {
+        for (let i = 0; i < popup.length; i++) {
+            if (target == popup[i].querySelector('form')) {
+                popup[i].appendChild(statusImg);
+                statusImg.src = source;
+                statusImg.style.cssText = `display: block; \
+                        max-width: 100%; \
+                        `;
+            }
+        }
+    };
+
+    let modalBlock = document.querySelector('.modal-block');
+
+    modalBlock.addEventListener('submit', e => {
+
+        let target = e.target;
+
+        e.preventDefault();
+        sendForm(target)
+            .then(() => {
+                statusMessge.innerHTML = "";
+                target.style.display = 'none';
+                putImg(target, "../src/img/okay.png");
+            })
+            .catch(() => {
+                statusMessge.innerHTML = "";
+                target.style.display = 'none';
+                putImg(target, "../src/img/browser.png");
+            })
+            .then(clearInput(target));
+        setTimeout(() => {
+            statusImg.remove();
+            target.style.display = 'block';
+        }, 5000);
     });
-
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].value = '';
-    }
-
-    return promise;
-  };
-
-  document.body.addEventListener('submit', e => {
-    
-    let target = e.target;
-    console.log(target);
-    
-    //submit - событие формы, поэтому все работает, несмотря
-    // на то, что цель кнопка
-    e.preventDefault();
-    sendForm(target)
-      .then(() => {
-        target.style.display = 'none';
-        popup.forEach(element => {
-          element.appendChild(img);
-        });
-        img.src = "../src/img/okay.png";
-        img.style.cssText = "display: block; \
-                            max-width: 100%; \
-                          ";
-      })
-      .catch(() => {
-        target.style.display = 'none';
-        popup.forEach(element => {
-          element.appendChild(img);
-        });
-        img.src = "../src/img/browser.png";
-        img.style.cssText = "display: block; \
-                            max-width: 100%; \
-                          ";
-      });
-    setTimeout(() => {
-      popup.removeChild(img);
-      target.style.display = 'block';
-    }, 5000);
-  });
 };
 
 module.exports = form;
